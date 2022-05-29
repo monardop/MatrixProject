b6a184b990c718f280e4bdd6117d04dce1117522
Pablo Monardo <pablomonardo@gmail.com>
2022-05-29 16:06:37 -0300

    Started to create the utilities section.

---
+++ MatrixProject/Utilities.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Utilities.h"
+
+int validValue(int bottom, int top, const char* msj) {
+    int value;
+    do{
+        printf(msj);
+        scanf("%d", &value);
+    }while((value < bottom) || (value > top));
+    return value;
+}
+void fillMatrix(float** Matrix, int typeValue, int row, int column) {
+    if(typeValue == realNumbers) {
+        float x;
+        for(int i = 0; i<row; i++){
+            for(int j = 0; j<column; j++){
+                printf("Insert a value for the r %d c %d: ", i , j);
+                scanf("%f", &x);
+                *(*(Matrix + i)+j) = x;
+            }
+        }
+    } else
+    {
+        int x;
+        for(int i = 0; i<row; i++){
+            for(int j = 0; j<column; j++){
+                printf("Insert a value for the r %d c %d: ", i , j);
+                scanf("%d", &x);
+                *(*(Matrix + i)+j) = x;
+            }
+        }
+    }
+    
+}
+void showRealsMatrix(float** M, int row, int column) {
+    for(int i = 0; i < row; i++) {
+        for(int j = 0; j< column; j++) {
+            printf("%.2f\t", *(*(M+i)+j));
+        }
+        printf("\n");
+    }    
+}
+void showIntMatrix(float** M, int row, int column) {
+    for(int i = 0; i < row; i++) {
+        for(int j = 0; j< column; j++) {
+            printf("%.0f\t", *(*(M+i)+j));
+        }
+        printf("\n");
+    }
+}
+void cleanMatrix(float** A, float** B, float** C) {
+    A = {{0}};
+    B = {{0}};
+    C = {{0}};
+}
+void menu() {
+    float (*sumFunctions[4])(int,int) = {"UpperTriangularSum","LowerTriangularSum", "trace", "secondaryTrace"};
+    void(*determineMatrixType[4])(int, int) = {"DiagonalMatrix", "SymmetricMatrix", "IdentityMatrix", "TransposeMatrix"};
+    void(*showMatrix[2])(float**, int, int) = {"showRealsMatrix","showIntMatrix"};
+}
---
+++ MatrixProject/Utilities.h
@@ -0,0 +1,11 @@
+#ifndef UTILITIES_H_INCLUDED
+#define UTILITIES_H_INCLUDED
+
+typedef enum values {realNumbers, integerNumber};
+float M1[5][5] = {{0}},M2[5][5] = {{0}},M3[5][5] = {{0}};
+
+int validValue(int, int,const char*);
+void fillMatrix(float**, int, int, int);
+void menu();
+
+#endif // UTILITIES_H_INCLUDED
