6a442aca5179c49042ea740d3d30f3539d8610a3
Pablo Monardo <pablomonardo@gmail.com>
2022-07-09 11:51:27 -0300

    Update Utilities.h

--- MatrixProject/Utilities.h
+++ MatrixProject/Utilities.h
@@ -4,7 +4,8 @@
 typedef enum values {realNumbers, integerNumber};
 float M1[5][5] = {{0}},M2[5][5] = {{0}},M3[5][5] = {{0}};
 int groupNumber;
-
+void showRealsMatrix(float** M, int row, int column); 
+void showIntMatrix(float** M, int row, int column);
 int validValue(int, int,const char*);
 void fillMatrix(float**, int, int, int);
 void menu();
